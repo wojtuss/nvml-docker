5a43f3f0d03223206d3d7c7411481471b4368dbc
Wojciech Uss <wojciech.uss@intel.com>
2016-05-24 14:33:56 +0200

    Corrected missing argument of build

--- buildFromScratch.sh
+++ buildFromScratch.sh
@@ -5,8 +5,8 @@ repo=wojtuss
 name=nvml-ubuntu1604:1
 tag=${repo}/${name}
 
-if [ -n $http_proxy ]; then BUILD_ARGS="$BUILD_ARGS --build-arg http_proxy=\$http_proxy"; fi
+if [ -n $http_proxy ]; then BUILD_ARGS="--build-arg http_proxy=\$http_proxy"; fi
 if [ -n $https_proxy ]; then BUILD_ARGS="$BUILD_ARGS --build-arg https_proxy=\$https_proxy"; fi
 
-docker build -t $tag $BUILD_ARGS
+docker build -t $tag $BUILD_ARGS .
 
