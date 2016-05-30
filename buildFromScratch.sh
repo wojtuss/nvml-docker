aa559eb5a207c8ddbcfa01df017ba1fd072a233e
Wojciech Uss <wojciech.uss@intel.com>
2016-05-30 15:29:54 +0200

    Handling http(s)_proxy variables corrected 5

--- buildFromScratch.sh
+++ buildFromScratch.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 set -e
 
+cp Dockerfile.template Dockerfile
+
 repo=nvml
 name=ubuntu:16.04
 tag=${repo}/${name}
 
-echo $http_proxy
-echo $https_proxy
-
-if [ ! -z "$http_proxy" ]; then echo $http_proxy; BUILD_ARGS=" --build-arg http_proxy=$http_proxy "; fi
-if [ ! -z "$https_proxy" ]; then echo $https_proxy; BUILD_ARGS="$BUILD_ARGS --build-arg https_proxy=$https_proxy "; fi
-
-echo $BUILD_ARGS
+if [ -n "$http_proxy" ]
+then
+	BUILD_ARGS=" --build-arg http_proxy=$http_proxy "
+	sed -i '/MAINTAINER.*/a ARG http_proxy' Dockerfile
+fi
+if [ -n "$https_proxy" ]
+then
+	BUILD_ARGS="$BUILD_ARGS --build-arg https_proxy=$https_proxy "
+	sed -i '/MAINTAINER.*/a ARG https_proxy' Dockerfile
+fi
 
 docker build -t $tag $BUILD_ARGS .
 
