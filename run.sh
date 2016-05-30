54553d574263ff8f0735c5a1c13065e4a355477d
Wojciech Uss <wojciech.uss@intel.com>
2016-05-30 16:16:38 +0200

    Handling http(s)_proxy variables corrected 7

--- run.sh
+++ run.sh
@@ -7,11 +7,12 @@ containerName=nvml-ubuntu-16.04
 if [[ $CC == "clang" ]]; then export CXX="clang++"; else export CXX="g++"; fi
 if [[ $MAKE_DPKG -eq 0 ]] ; then command="/bin/bash ./make.sh"; fi
 if [[ $MAKE_DPKG -eq 1 ]] ; then command="/bin/bash ./make_dpkg.sh"; fi
-echo $command
+
+if [ -n "$http_proxy" ]; then RUN_OPTIONS=" $RUN_OPTIONS --env http_proxy=$http_proxy "; fi
+if [ -n "$https_proxy" ]; then RUN_OPTIONS=" $RUN_OPTIONS --env https_proxy=$https_proxy "; fi
 
 docker run --rm --privileged=true --dns=172.28.168.170 --name=$containerName -ti \
-	--env http_proxy=$http_proxy \
-	--env https_proxy=$https_proxy \
+	$RUN_OPTIONS \
 	--env CC=$CC \
 	--env CXX=$CXX \
 	--env EXTRA_CFLAGS=$EXTRA_CFLAGS \
