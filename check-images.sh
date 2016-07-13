a54b3576dc32059404d9b75bf310158b2ae27faf
Wojciech Uss <wojciech.uss@intel.com>
2016-07-13 12:30:46 +0200

    ubuntu2

--- check-images.sh
+++ check-images.sh
@@ -12,8 +12,7 @@ base_dir=testdir
 for file in $files; do
 	echo file: $file
 
-	if [[ $file =~ "^($base_dir)\/Dockerfile\.$OS-$OS_VER" \
-		|| ( $file =~ "^($base_dir)\/" && ! $file =~ "^($base_dir)\/Dockerfile\." ) ]]; then
+	if [[ $file =~ ^($base_dir)\/Dockerfile\.($OS)-($OS_VER)$ ]] || ( [[ $file =~ ^($base_dir)\/ ]] && [[ ! $file =~ ^($base_dir)\/Dockerfile\. ]] ); then
 		echo build $OS:$OS_VER
 		if [[ ! $TRAVIS_PULL_REQUEST ]]; then
 			echo push image to docker hub
