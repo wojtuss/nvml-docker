ad7ffbf3f21815cf83c95b6d80a2bca56fd13be9
Wojciech Uss <wojciech.uss@intel.com>
2016-07-13 11:31:06 +0200

    base dir handled

--- check-images.sh
+++ check-images.sh
@@ -12,20 +12,23 @@ echo files: $files
 #my="Dockerfile.ubuntu-16.04 Dockerfile.fedora-23"
 #echo my: $my
 
+base_dir=testdir
+
 for file in $files; do
 	echo file: $file
-	if [[ $file =~ ^testdir\/Dockerfile\. ]]; then
-		if [[ $file =~ ^testdir\/Dockerfile\.$OS-$OS_VER ]]; then
+
+	if [[ $file =~ ^($base_dir)\/ ]]; then
+		if [[ $file =~ ^($base_dir)\/Dockerfile\.$OS-$OS_VER ]]; then
 			echo Dockerfile, build: $OS:$OS_VER
 			if [[ ! $TRAVIS_PULL_REQUEST ]]; then
 				echo push image to docker hub
 			else
 				echo skip pushing docker image
 			fi
+		else
+			echo other, build: $OS:$OS_VER
+			break
 		fi
-	else
-		echo other, build: $OS:$OS_VER
-		break
 	fi
 done
 
