64e30828781f3b74fedab2bcfadc336d65d1971a
Wojciech Uss <wojciech.uss@intel.com>
2016-07-13 13:41:14 +0200

    other

--- check-images.sh
+++ check-images.sh
@@ -12,7 +12,9 @@ base_dir=testdir
 for file in $files; do
 	echo file: $file
 
-	if [[ $file =~ ^($base_dir)\/Dockerfile\.($OS)-($OS_VER)$ ]] || ( [[ $file =~ ^($base_dir)\/ ]] && [[ ! $file =~ ^($base_dir)\/Dockerfile\. ]] ); then
+	if [[ $file =~ ^($base_dir)\/Dockerfile\.($OS)-($OS_VER)$ ]] \
+		|| [[ $file =~ ^($base_dir)\/.*\.sh$ ]]
+	then
 		echo build $OS:$OS_VER
 		if [[ $TRAVIS_EVENT_TYPE == "pull_request" ]]; then
 			echo skip pushing docker image
