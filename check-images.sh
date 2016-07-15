1f752cd961991ca93e65702214471b29d0f902b9
Wojciech Uss <wojciech.uss@intel.com>
2016-07-15 09:44:35 +0200

    modified

--- check-images.sh
+++ check-images.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-# [[ $TRAVIS_REPO_SLUG == "wojtuss/nvml-docker" && $TRAVIS_BRANCH == "master" ]] || echo 0 && exit 0
+gitrepo=wojtuss/nvml-docker
+gitbranch=master
+# [[ $TRAVIS_REPO_SLUG == $gitrepo && $TRAVIS_BRANCH == $gitbranch ]] \
+#	|| echo "Nothing to check (the commit is not for $gitbranch branch of $gitrepo." && exit 0
 
 commitRange=$([[ -n "$TRAVIS_COMMIT_RANGE" ]] && echo ${TRAVIS_COMMIT_RANGE/\.\.\./ } || echo $TRAVIS_COMMIT)
+echo "Commit range: $commitRange"
+
 files=$(git diff-tree --no-commit-id --name-only -r $commitRange)
+echo "Files modified within the commit range:"
+for file in $files; do echo $file; done
+
 base_dir=testdir
 
 for file in $files; do
-
 	if [[ $file =~ ^($base_dir)\/Dockerfile\.($OS)-($OS_VER)$ ]] \
 		|| [[ $file =~ ^($base_dir)\/.*\.sh$ ]]
 	then
+		echo "Rebuilding Docker image for Dockerfile\.($OS)-($OS_VER)"
 		cd testdir && ./build-image.sh $OS:$OS_VER
-		[[ $TRAVIS_EVENT_TYPE == "pull_request" ]] && echo 0 || echo 1
+		[[ $TRAVIS_EVENT_TYPE == "pull_request" ]] || touch push_new_Docker_image_to_repo
 		exit 0
 	fi
 done
 
-echo 0
+echo "The Docker image does not need rebuilding."
